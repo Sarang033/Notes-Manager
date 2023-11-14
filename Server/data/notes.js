01cd5c61ccbb61af89d9b8a8ffd3f514ed917593
Sarang Sharma <sarangchamp2--4>
2023-11-14 08:22:25 +0530

    created a server

---
+++ Server/data/notes.js
@@ -0,0 +1,22 @@
+const notes =[
+    {
+        _id:"1",
+        title:"Day 1 of college",
+        content:"I made a few new friends and introduced myself to teachers",
+        category:"College"
+    },
+    {
+        _id:"2",
+        title:"Learned some NodeJS",
+        content:"I made a server using express",
+        category:"Learning"
+    },
+    {
+        _id:"3",
+        title:"Movie day with frineds",
+        content:"I watched The Marvels with my frineds",
+        category:"Movie"
+    },
+]
+
+module.exports = notes;
